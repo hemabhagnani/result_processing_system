aefe2aa16cdbe5f55951c025c7c3e32634888f9c
Hema bhagnani <hemabhagnani5@gmail.com>
2020-03-14 20:19:16 +0530

    dropdowns

--- job/views.py
+++ job/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request,'job/home.html')
+
+def dept_form(request):
+    if request.method=='POST':
+        Department=department()
+        Department.dept_name=request.POST['dept_name']
+        Department.dept_no=request.POST['dept_no']
+        Department.save()
+        return redirect('dept_form')
+
+    else:
+       return render(request,'job/home.html')
+
+def student_form(request):
+    if request.method=='POST':
+        Student=student()
+        Student.enroll_no=request.POST['enroll_no']
+        Student.fname=request.POST['fname']
+        Student.lname=request.POST['lname']
+        Student.dept_no=request.POST['dept_no']
+
+        Student.save()
+        return redirect('')
+
+    else:
+       return render(request,'job/home.html')
--- rps/urls.py
+++ rps/urls.py
@@ -21,5 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',job.views.home,name='home')
+    path('',job.views.home,name='home'),
+    path('dept_form/',job.views.dept_form,name='dept_form'),
+    path('student_form/',job.views.student_form,name='student_form'),
+
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
