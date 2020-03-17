50264ce949f248842bc50e4694a60197445a6eee
hemabhagnani <42410728+hemabhagnani@users.noreply.github.com>
2020-03-17 10:06:55 +0530

    Update models.py

--- job/models.py
+++ job/models.py
@@ -5,42 +5,53 @@ class department(models.Model):
     dept_no=models.IntegerField(primary_key=True)
     dept_name=models.TextField(max_length=50)
 
+
 class student(models.Model):
     enroll_no=models.TextField(primary_key=True,max_length=20)
-    dept_no=models.ForeignKey(department,on_delete=models.CASCADE)
+    student_dept_no=models.ForeignKey(department,on_delete=models.CASCADE)
     fname=models.TextField(max_length=50)
     lname=models.TextField(max_length=50)
 
+
+
+
+
 class scheme(models.Model):
     scheme_no=models.IntegerField(primary_key=True)
-    theory_cr=models.IntegerField(max_length=5)
-    practical_cr=models.IntegerField(max_length=5)
-    max_th=models.IntegerField(max_length=5)
-    max_pr=models.IntegerField(max_length=5)
-    max_mid=models.IntegerField(max_length=5)
+    theory_cr=models.IntegerField()
+    practical_cr=models.IntegerField()
+    max_th=models.IntegerField()
+    max_pr=models.IntegerField()
+    max_mid=models.IntegerField()
+
+
+
+
+
 
 class subjects(models.Model):
     sub_no=models.IntegerField(primary_key=True)
-    dept_no=models.ForeignKey(department,on_delete=models.CASCADE)
-    scheme_no=models.ForeignKey(scheme,on_delete=models.CASCADE)
-    
+    sub_name=models.TextField(max_length=50,default="name")
+    sub_dept_no=models.ForeignKey(department,on_delete=models.CASCADE)
+    sub_scheme_no=models.ForeignKey(scheme,on_delete=models.CASCADE)
+    Sem=models.IntegerField()
 
 
 
 
 class marks(models.Model):
-    enroll_no=models.ForeignKey(student,on_delete=models.CASCADE)
-    sub_no=models.ForeignKey(subjects,on_delete=models.CASCADE)
-    th=models.IntegerField(max_length=5)
-    pr=models.IntegerField(max_length=5)
-    mid=models.IntegerField(max_length=5)
+    marks_enroll_no=models.ForeignKey(student,on_delete=models.CASCADE)
+    marks_sub_no=models.ForeignKey(subjects,on_delete=models.CASCADE)
+    th=models.IntegerField()
+    pr=models.IntegerField()
+    mid=models.IntegerField()
 
 class backlog(models.Model):
-    enroll_no=models.ForeignKey(student,on_delete=models.CASCADE)
-    sub_no=models.ForeignKey(subjects,on_delete=models.PROTECT)
+    back_enroll_no=models.ForeignKey(student,on_delete=models.CASCADE)
+    back_sub_no=models.ForeignKey(subjects,on_delete=models.PROTECT)
 
 class result(models.Model):
-    enroll_no=models.ForeignKey(student,on_delete=models.CASCADE)
+    result_enroll_no=models.ForeignKey(student,on_delete=models.CASCADE)
     sem1=models.FloatField(default='0',max_length=5)
     sem2=models.FloatField(default='0',max_length=5)
     sem3=models.FloatField(default='0',max_length=5)
